75463517451538b62e2b7f3eaced0c2f84efb861
agataokolska <agata.okolska@gmail.comgit config --global user.name Agata>
2018-02-24 12:56:14 +0100

    functions with parameter

--- script.js
+++ script.js
@@ -1,54 +1,12 @@
-console.log('Hello Agata!')
-//data types
-
-//primitives
-
-//string
-var textVariable = "Mateusz";
-console.log(textVariable)
-
-//number
-var numberVariable = 55
-console.log(numberVariable)
-
-//boolean
-var booleanVariable = true
-
-//null
-var nullVariable = null
-
-//undefined
-var undefinedVariable = undefined
-
-//objects
-// objects with data
-var simplestObj = {
-    name: "Agata",
-    lastname: "Okolska"
+// function declaration
+function hello(name){
+    console.log("Hello " + name + "!")
+    console.log("Hello " + name + "!")
+    console.log("Hello " + name + "!")
 }
-console.log(simplestObj)
+//function call
 
-//nested objects with data
-var nestedObj = {
-    name: "Agata",
-    car: {
-        brand: "Peugeot",
-        model: 407
-    }
-}
-console.log(nestedObj)
-console.log(nestedObj.car.model)
-console.log(nestedObj.car.year)
+hello("Mateusz") //passing attribute Mateusz
+hello("Ala")
+hello("Ela")
 
-// arrays
-
-var simpleArray = [1,2,3,4,5]
-console.log(simpleArray)
-var simpleArray2 = [1,
-    {name:"Ala", array: [1,2,3]}
-    ,3,4,5]
-console.log(simpleArray2)
-console.log(simpleArray2[4])
-
-console.log(simpleArray2[1].name)
-console.log(simpleArray2[1].['name'])
