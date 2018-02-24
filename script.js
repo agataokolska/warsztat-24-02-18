de6c7ee5d60884edd4f2f509ddea239095a5a894
agataokolska <agata.okolska@gmail.comgit config --global user.name Agata>
2018-02-24 12:23:56 +0100

    primitives data types

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 console.log('Hello Agata!')
+//data types
+
+//primitives
+
+//string
+var textVariable = "Mateusz";
+console.log(textVariable)
+
+//number
+var numberVariable = 55
+console.log(numberVariable)
+
+//boolean
+var booleanVariable = true
+
+//null
+var nullVariable = null
+
+//undefined
+var undefinedVariable = undefined
