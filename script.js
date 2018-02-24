e38727e862a3edf1ec1974a679d423de30c9c6ee
agataokolska <agata.okolska@gmail.comgit config --global user.name Agata>
2018-02-24 15:44:18 +0100

    Make mole and add point when clicked

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 (function(){
+    function addPoint(){     
+        console.log('Dodano punkt!')  
+    }
 
     function makeMole(){
 
         var molePosX = Math.round(
-            Math.random() * (window.innerWidth - window.innerHeight / 10)
+            Math.random() * (window.innerWidth - window.innerHeight / 10) //1/10 viewport width i 1/10 vewport height
         )
         var molePosY = Math.round(
             Math.random() * (window.innerHeight - window.innerHeight / 10)
         )
+
         var mole = document.createElement('div')
 
         mole.classList.add('mole') //dodano klase css od mole
@@ -15,7 +19,15 @@
         mole.style.left = molePosX + 'px'
         mole.style.top = molePosY + 'px'
 
-        document.querySelector('body').appendChild(mole)
+        mole.addEventListener(
+            'click',
+            function(){
+                mole.remove()  //po kliknieciu w kreta usuń go
+                addPoint()   
+            }
+        )
+
+        document.querySelector('body').appendChild(mole) // adding mole to body
     }
 
 makeMole()
