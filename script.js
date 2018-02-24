281a18b2d0d0cc7e87a60fe8912b648db6de4851
agataokolska <agata.okolska@gmail.comgit config --global user.name Agata>
2018-02-24 12:42:51 +0100

    objects and arrays

--- script.js
+++ script.js
@@ -19,3 +19,36 @@ var nullVariable = null
 
 //undefined
 var undefinedVariable = undefined
+
+//objects
+// objects with data
+var simplestObj = {
+    name: "Agata",
+    lastname: "Okolska"
+}
+console.log(simplestObj)
+
+//nested objects with data
+var nestedObj = {
+    name: "Agata",
+    car: {
+        brand: "Peugeot",
+        model: 407
+    }
+}
+console.log(nestedObj)
+console.log(nestedObj.car.model)
+console.log(nestedObj.car.year)
+
+// arrays
+
+var simpleArray = [1,2,3,4,5]
+console.log(simpleArray)
+var simpleArray2 = [1,
+    {name:"Ala", array: [1,2,3]}
+    ,3,4,5]
+console.log(simpleArray2)
+console.log(simpleArray2[4])
+
+console.log(simpleArray2[1].name)
+console.log(simpleArray2[1].['name'])
