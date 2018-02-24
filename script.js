c836a3fd02eb854cd71fa25098d2c20669eb14be
agataokolska <agata.okolska@gmail.comgit config --global user.name Agata>
2018-02-24 15:21:59 +0100

    Mole position

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 (function(){
 
     function makeMole(){
+
+        var molePosX = 100
+        var molePosY = 100
         var mole = document.createElement('div')
 
         mole.classList.add('mole') //dodano klase css od mole
 
-        mole.style.left = '100px'
-        mole.style.top = '100px'
+        mole.style.left = molePosX + 'px'
+        mole.style.top = molePosY + 'px'
 
         document.querySelector('body').appendChild(mole)
     }
