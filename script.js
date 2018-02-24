b6bbf117b850663a167fb1f31be41c09d25575f6
agataokolska <agata.okolska@gmail.comgit config --global user.name Agata>
2018-02-24 15:19:56 +0100

    GAME - first commit

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
-var body = document.querySelector('body')
+(function(){
 
-console.log(body)
+    function makeMole(){
+        var mole = document.createElement('div')
 
-body.addEventListener(
-    'click',
-    function() {
-        console.log("Klik!")
+        mole.classList.add('mole') //dodano klase css od mole
+
+        mole.style.left = '100px'
+        mole.style.top = '100px'
+
+        document.querySelector('body').appendChild(mole)
     }
-)
+
+makeMole()
+
+})()
