3bd1c05722d8475fb47d910c4e319764c81b0600
agataokolska <agata.okolska@gmail.comgit config --global user.name Agata>
2018-02-24 13:05:38 +0100

    function  for

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 // function declaration
 function hello(name){
+    for(var i = 0; i < 3; i++) {
     console.log("Hello " + name + "!")
-    console.log("Hello " + name + "!")
-    console.log("Hello " + name + "!")
+    }
 }
 //function call
 
